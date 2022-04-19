001a6f93b7fb37e53dd7ee9517f3310f02ea64c5
SodaCris <wang-jiaxun@outlook.com>
2022-04-19 13:31:46 +0800

    add code

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"github.com/fatih/color"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	argsWithProg := os.Args
+	if len(argsWithProg) != 2 {
+		fmt.Println("Usage: w <name>")
+		return
+	}
+	paths := strings.Split(os.Getenv("PATH"), ";")
+	found := 0
+	for _, path := range paths {
+		if fileinfo, err := os.Stat(path); err == nil {
+			if fileinfo.IsDir() {
+				files, err := ioutil.ReadDir(path)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				for _, file := range files {
+					name := filepath.Base(file.Name())
+					ext := filepath.Ext(file.Name())
+					base := strings.TrimSuffix(name, ext)
+					if base == argsWithProg[1] {
+						fmt.Print(path)
+						fmt.Printf("%c", os.PathSeparator)
+						color.Set(color.FgGreen)
+						fmt.Print(base)
+						color.Unset()
+						fmt.Println(ext)
+						found += 1
+					}
+				}
+			}
+		}
+	}
+	if found == 0 {
+		color.Red("Command not found.\n")
+	}
+}
