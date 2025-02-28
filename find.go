14564d94c8a3d074431e54430e165778f723874a
sodacris <wjxa20152015@gmail.com>
2025-02-28 15:48:21 +0800

    refactor into smaller functions

--- find.go
+++ find.go
@@ -1,41 +1,44 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 )
 
+func FindFiles(path string, files []os.DirEntry, command string) []Path {
+	var ret []Path
+	for _, file := range files {
+		name := filepath.Base(file.Name())
+		ext := filepath.Ext(file.Name())
+		base := strings.TrimSuffix(name, ext)
+
+		if strings.EqualFold(base, command) {
+			var p Path
+			p.Dir = path
+			p.File = file.Name()
+			p.Basename = base
+			p.Suffix = ext
+			ret = append(ret, p)
+		}
+	}
+	return ret
+}
+
 func FindCommand(paths []string, command string) List[Path] {
 	var found []Path
 	for _, path := range paths {
 		fileinfo, err := os.Stat(path)
-		if err != nil {
-			continue
-		}
-		if !fileinfo.IsDir() {
+		if err != nil || !fileinfo.IsDir() {
 			continue
 		}
 		files, err := os.ReadDir(path)
 		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
+			continue
 		}
-		for _, file := range files {
-			name := filepath.Base(file.Name())
-			ext := filepath.Ext(file.Name())
-			base := strings.TrimSuffix(name, ext)
 
-			if strings.EqualFold(base, command) {
-				var p Path
-				p.Dir = path
-				p.File = file.Name()
-				p.Basename = base
-				p.Suffix = ext
-				found = append(found, p)
-			}
-		}
+		results := FindFiles(path, files, command)
+		found = append(found, results...)
 	}
 	return found
 }
