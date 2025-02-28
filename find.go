46c342d42701c1a1dc7d293d0ca56507124da93f
sodacris <wjxa20152015@gmail.com>
2025-02-28 14:58:38 +0800

    split 1

---
+++ find.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	
+	"github.com/fatih/color"
+)
+
+func FindCommand(paths []string, command string) int {
+	found := 0
+	for _, path := range paths {
+		if fileinfo, err := os.Stat(path); err == nil {
+			if fileinfo.IsDir() {
+				files, err := ioutil.ReadDir(path)
+				if err != nil {
+					fmt.Println(err)
+					os.Exit(1)
+				}
+				for _, file := range files {
+					name := filepath.Base(file.Name())
+					ext := filepath.Ext(file.Name())
+					base := strings.TrimSuffix(name, ext)
+					if strings.EqualFold(base, command) {
+						fmt.Print(path)
+						fmt.Printf("%c", os.PathSeparator)
+						color.Set(color.FgGreen)
+						fmt.Print(base)
+						color.Unset()
+						fmt.Println(ext)
+						found += 1
+					}
+				}
+			}
+		}
+	}
+	return found
+}
